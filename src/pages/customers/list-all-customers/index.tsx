b6887d9ca707d605c360d6fcf5a26895df4e1282
Chejohrpp <sergioroherp@gmail.com>
2024-03-09 00:07:11 -0600

    create table list for customers with a dump data for only purpose to see how it works, not connected with backend

--- src/pages/customers/list-all-customers/index.tsx
+++ src/pages/customers/list-all-customers/index.tsx
@@ -3,30 +3,61 @@ import Grid from '@mui/material/Grid'
 import Link from '@mui/material/Link'
 import Card from '@mui/material/Card'
 import Typography from '@mui/material/Typography'
-import CardHeader from '@mui/material/CardHeader'
 
 // ** Demo Components Imports
-import TableBasic from 'src/views/tables/TableBasic'
+import EnhancedTable from './components/TableCustomer'
 
-const ListAllCustomer = () => {
+const ListAllCustomer = ({ data }: any) => {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
         <Typography variant='h5'>
           <Link href='https://mui.com/components/tables/' target='_blank'>
-            Lista de los Compradores
+            Listado de Compradores
           </Link>
         </Typography>
         <Typography variant='body2'>Listado general de todos los compradores</Typography>
       </Grid>
       <Grid item xs={12}>
         <Card>
-          <CardHeader title='Basic Table' titleTypographyProps={{ variant: 'h6' }} />
-          <TableBasic />
+          <EnhancedTable dataServer={data} />
         </Card>
       </Grid>
     </Grid>
   )
+}
+
+
+export async function getServerSideProps(context: any) {
+  //fetch de my server de spring
+  // const response  =  await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  // const data  = await response.json()
+  // console.log(data)
+
+  return {
+    props: {
+      data : [
+        {
+          id_user: 1,
+          nit: 123456789,
+          name: 'Juan',
+          phone: 12345678,
+          dpi: 1234567890123,
+          email: 'user1@example.com',
+          address : '543 Plaza Trail'
+        },
+        {
+          "id_user": 2,
+          "nit": 123456799,
+          "name": "Gilberto",
+          "phone": 55891123,
+          "dpi": 1234567890333,
+          "email": "user2@example.com",
+          "address": "789 Turkey jhonson"
+        }
+      ]
+    }
+  }
 }
 
 export default ListAllCustomer
