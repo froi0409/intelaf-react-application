38e12aa8ac90c1b50c5aa14e7e0a21b32cad1f56
froi0409 <ferrubocana@gmail.com>
2024-03-12 15:48:14 -0600

    Edit store has been completed

--- src/pages/edit-store/index.tsx
+++ src/pages/edit-store/index.tsx
@@ -6,23 +6,22 @@ import Alert from '@mui/material/Alert';
 
 import axios from 'axios';
 
-interface State {
-    password: string
-    showPassword: boolean
-}
-
-// Styled component for the form
 const Form = styled('form')(({ theme }) => ({
     maxWidth: 400,
     padding: theme.spacing(12),
     borderRadius: theme.shape.borderRadius,
     border: `1px solid ${theme.palette.divider}`
 }))
-const CreateStoreLayout = () => {
+const EditStoreLayout = () => { 
 
     const [response, setResponse] = useState('');
-    const [messageType, setMessageType] = useState('')
+
+    const [findMessageType, setFindMessageType] = useState('');
+    const [findSubmitted, setFindSubmitted] = useState(false);
+    const [messageType, setMessageType] = useState('');
     const [submitted, setSubmitted] = useState(false);
+
+    const [isDisabled, setIsDisabled] = useState(true)
 
     const [storeCode, setStoreCode] = useState('');
     const [storeName, setStoreName] = useState('');
@@ -33,9 +32,11 @@ const CreateStoreLayout = () => {
     const [openingHour, setOpeningHour] = useState('');
     const [closingHour, setClosingHour] = useState('');
 
-    const handleSubmit = async (e: React.FormEvent) => {
+    const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+    }
 
+    const handleEdit = async () => {
         const formData = {
             idStore: storeCode,
             name: storeName,
@@ -46,70 +47,157 @@ const CreateStoreLayout = () => {
             openingHour: openingHour,
             closingHour: closingHour
         };
-        
+
         try {
-            const res = await axios.post('http://localhost:8080/v1/store', formData);
+            const res = await axios.put('http://localhost:8080/v1/store', formData);
 
             setSubmitted(true);
+            setFindSubmitted(false);
 
-            if (res.status === 201) {
-                setResponse('La tienda fue creada con éxito');
-                setMessageType('OK')
+
+            if (res.status === 200) {
+                setMessageType('OK');
+                setIsDisabled(true);
+                setResponse(`Los datos de la tienda ${storeCode} fueron actualizados con éxito`)
             } else {
-                setResponse('la tienda no pudo ser creada');
-                setMessageType('ERR')
+                setMessageType('ERR');
+                setResponse('La tienda no pudo ser actualizada');
             }
+
         } catch (error) {
             setSubmitted(true);
-            setMessageType('ERR')
+            setFindSubmitted(false);
 
-            if (axios.isAxiosError(error) && error.response && error.response.status === 400) {
-                setResponse('El código ya está asociado a otra tienda, intenta con otro código');
+            if (axios.isAxiosError(error) && error.response && error.response.status === 404) {
+                setResponse('No existe ninguna tienda con el código especificado');
             } else {
-                setResponse('Ocurrió un error al enviar la solicitud');
+                setResponse('Ocurrió un error al enviar al actualizar la tienda');
             }
         }
 
     }
 
+    const handleSearch = async () => {
+        try {
+            const res = await axios.get(`http://localhost:8080/v1/store/${storeCode}`);
+
+            setFindSubmitted(true);
+            setSubmitted(false);
+
+            if (res.status === 200) {
+                setFindMessageType('OK');
+                setStoreName(res.data.name);
+                setStoreAddress(res.data.address);
+                setStoreEmail(res.data.email);
+                setStorePhone1(res.data.phone1);
+                setIsDisabled(false);
+
+                if (res.data.phone2 != null) {
+                    setStorePhone2(res.data.phone2);
+                } else {
+                    setStorePhone2('');
+                }
+
+                if (res.data.openingHour != null) {
+                    setOpeningHour(res.data.openingHour);
+                } else {
+                    setOpeningHour('');
+                }
+
+                if (res.data.closingHour != null) {
+                    setClosingHour(res.data.closingHour);
+                } else {
+                    setClosingHour('');
+                }
+            } else {
+                setResponse('No se pudo encontrar información de la tienda')
+            }
+        } catch (error) {
+            setFindSubmitted(true);
+            setSubmitted(false);
+            setFindMessageType('ERR');
+
+            clearInput();
+
+            if (axios.isAxiosError(error) && error.response && error.response.status === 404) {
+                setResponse('No existe ninguna tienda con el código especificado');
+            } else {
+                setResponse('Ocurrió un error al enviar la solicitud');
+            }
+        }
+    }
+
+    const clearInput = () => {
+        //setStoreCode('');
+        setStoreName('');
+        setStoreAddress('');
+        setStoreEmail('');
+        setStorePhone1('');
+        setStorePhone2('');
+        setOpeningHour('');
+        setClosingHour('');
+    }
+
     return (
         <Card>
             <Grid>
-                {submitted && response != '' && (
+                {findSubmitted && response != '' && (
                     <div>
-                        {messageType === 'OK' ? (
-                            <Alert severity="success">{response}</Alert>
-                        ) : (
+                        {findMessageType === 'ERR' && (
                             <Alert severity="error">{response}</Alert>
                         )
-
                         }
                     </div>
-                )}
+                )
+                }
+                {submitted && response != '' && (
+                    <div>
+                        {
+                            messageType === 'OK' ? (
+                                <Alert severity="success">{response}</Alert>
+                            ) : (
+                                <Alert severity="error">{response}</Alert>
+                            )
+                        }
+                    </div>
+                )
+
+                }
             </Grid>
-            <CardHeader title='Crear Tienda' titleTypographyProps={{ variant: 'h6' }} />
+            <CardHeader title='Editar Tienda' titleTypographyProps={{ variant: 'h6' }} />
             <CardContent sx={{ minHeight: 500, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <form onSubmit={handleSubmit}>
+                <form>
                     <Grid container spacing={5}>
                         <Grid item xs={12}>
-                        <TextField
-                            fullWidth
-                            label='Código de la Tienda'
-                            placeholder='STR-#'
-                            required
-                            value={storeCode}
-                            onChange={(e) => setStoreCode(e.target.value)}
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position='start'>
-                                    <CardAccountDetailsOutline />
-                                    </InputAdornment>
-                                )
-                            }}
-                        />
+                            <Grid container spacing={5} alignItems='center'>
+                                <Grid item xs={10}>
+                                <TextField
+                                    disabled={!isDisabled}
+                                    fullWidth
+                                    label='Código de la Tienda'
+                                    placeholder='STR-#'
+                                    required
+                                    value={storeCode}
+                                    onChange={(e) => setStoreCode(e.target.value)}
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position='start'>
+                                            <CardAccountDetailsOutline />
+                                            </InputAdornment>
+                                        )
+                                    }}
+                                />
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Button disabled={!isDisabled} onClick={handleSearch} variant='contained' size='large' fullWidth>
+                                        Buscar Tienda
+                                    </Button>
+                                </Grid>
+                            </Grid>
                         </Grid>
                         <Grid item xs={12}>
                         <TextField
+                            disabled={isDisabled}
                             fullWidth
                             label='Nombre de la Tienda'
                             placeholder='Ej: Interplaza Xela'
@@ -127,6 +215,7 @@ const CreateStoreLayout = () => {
                         </Grid> 
                         <Grid item xs={12}>
                         <TextField
+                            disabled={isDisabled}
                             fullWidth
                             label='Dirección de la Tienda'
                             placeholder='Ej: 14 avenida, zona 1, Centro Comercial Interplaza Xela, Local 25'
@@ -144,6 +233,7 @@ const CreateStoreLayout = () => {
                         </Grid>
                         <Grid item xs={12}>
                         <TextField
+                            disabled={isDisabled}
                             fullWidth
                             type='email'
                             label='Correo Electrónico'
@@ -163,6 +253,7 @@ const CreateStoreLayout = () => {
                         </Grid>
                         <Grid item xs={12}>
                         <TextField
+                            disabled={isDisabled}
                             fullWidth
                             type='number'
                             label='Teléfono 1'
@@ -171,20 +262,17 @@ const CreateStoreLayout = () => {
                             value={storePhone1}
                             onChange={(e) => { setStorePhone1(e.target.value) }}
                             InputProps={{
-                                inputProps: {
-                                    min: 0,
-                                    max: 99999999
-                                },
-                                startAdornment: (
-                                    <InputAdornment position='start'>
-                                    <Phone />
-                                    </InputAdornment>
-                                )
+                            startAdornment: (
+                                <InputAdornment position='start'>
+                                <Phone />
+                                </InputAdornment>
+                            )
                             }}
                         />
                         </Grid>
                         <Grid item xs={12}>
                         <TextField
+                            disabled={isDisabled}
                             fullWidth
                             type='number'
                             label='Teléfono 2'
@@ -204,6 +292,7 @@ const CreateStoreLayout = () => {
                             <Grid container spacing={5}>
                             <Grid item xs={6}>
                                 <TextField
+                                    disabled={isDisabled}
                                     fullWidth
                                     type='time'
                                     label='Horario Apertura'
@@ -221,6 +310,7 @@ const CreateStoreLayout = () => {
                                 </Grid>
                                 <Grid item xs={6}>
                                 <TextField
+                                    disabled={isDisabled}
                                     fullWidth
                                     type='time'
                                     label='Horario de Cierre'
@@ -241,10 +331,11 @@ const CreateStoreLayout = () => {
                         <Grid item xs={12}>
                             <Grid container spacing={5}>
                                 <Grid item xs={2}>
-                                    <Button type='submit' variant='contained' size='large' fullWidth>
-                                        Crear Tienda
+                                    <Button type="button" onClick={handleEdit} variant='contained' size='large' fullWidth>
+                                        Editar Tienda
                                     </Button>
                                 </Grid>
+
                                 <Grid item xs={10}>
                                     
                                 </Grid>
@@ -257,5 +348,4 @@ const CreateStoreLayout = () => {
     )
 }
 
-export default CreateStoreLayout
-
+export default EditStoreLayout
