9c78f18afe8e7600a89cce17c3bc90944380f779
Chejohrpp <sergioroherp@gmail.com>
2024-04-03 17:57:09 -0600

    traking order done, still jwt get username

---
+++ src/utils/apiUtils/order/trakingsOrders.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+//@ts-ignore
+import { getCookieJwt } from 'src/utils/cookieUtils';
+
+
+export async function getTrakingsOrders(): Promise<any> {
+    try {
+      const response = await axios.get('/api/order/trakingOrders/', {
+        headers: {
+            // Authorization: getCookieJwt()
+        }
+    })
+      return response.data;
+    } catch (error) {
+        console.log(error);
+      throw new Error('Error to get the trakings orders');
+    }
+  }
